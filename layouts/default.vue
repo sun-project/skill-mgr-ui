be25e31e5d8ce924feba96a9506d00f535f73df7
Takeshi Miyajima <tmiyajima@sunarch.co.jp>
2019-03-15 02:23:50 +0900

    ヘッダーにユーザー名を表示

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,33 @@
 <template>
   <a-layout class="layout-default">
     <a-layout-header>
-      スーパー経歴管理システム
+      <a-row
+        justify="space-between"
+        type="flex"
+      >
+        <a-col>
+          SkillMgr - 経歴管理
+        </a-col>
+        <a-col v-if="$route.params.userId">
+          <a-dropdown placement="bottomRight">
+            <a-menu slot="overlay">
+              <a-menu-item>
+                <nuxt-link to="/">
+                  ログアウト
+                </nuxt-link>
+              </a-menu-item>
+            </a-menu>
+            <div style="cursor: pointer;">
+              <a-avatar
+                icon="user"
+                size="small"
+              />
+              {{ $route.params.userId }}
+              <a-icon type="down" />
+            </div>
+          </a-dropdown>
+        </a-col>
+      </a-row>
     </a-layout-header>
     <a-layout-content>
       <nuxt />
